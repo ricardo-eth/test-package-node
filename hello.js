5dedef9a509d3cf05698034e502a1ff579a75f86
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-03-11 12:51:54 +0100

    Update

--- hello.js
+++ hello.js
@@ -1,5 +1,13 @@
 const chalk = require('chalk')
+var readlineSync = require('readline-sync');
+
+const userName = readlineSync.question('May I have your name? ');
+let ageStr = readlineSync.question('May i have your age? ');
+
+age = Number(ageStr)
+console.log(typeof age)
+
 
 for (let i = 0; i < 10; ++i) {
-  console.log(chalk.red.underline.bold('Hello HardFork!'))
+  console.log(chalk.red.rgb(10,100 + i * 10, 200 )(`Hello ${userName} !`))
 }
